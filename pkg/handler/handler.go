3b00931aea40f1cb66648056f8cced043beafc70
Jemal-bashimovna <rozynyazowajemal@gmail.com>
2024-09-23 11:43:42 +0500

    declared structure Handler with method InitRoutes in pkg/handler/handler.go . Added endpoints POST/tasks, GET/tasks, PUT/tasks and DELETE/tasks

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,1 +1,18 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct{}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	tasks := router.Group("tasks")
+	{
+		tasks.POST("/")
+		tasks.GET("/")
+		tasks.PUT("/:id")
+		tasks.DELETE("/:id")
+	}
+	return router
+}
