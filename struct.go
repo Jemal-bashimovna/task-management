fb3dd51128518d1d98cb97308ea99537efe2f38c
Jemal-bashimovna <rozynyazowajemal@gmail.com>
2024-09-24 17:12:31 +0500

    update Taskd complited

--- struct.go
+++ struct.go
@@ -1,5 +1,7 @@
 package taskmanagement
 
+import "errors"
+
 type Tasks struct {
 	Id          int    `json:"id" db:"id"`
 	Title       string `json:"title" binding:"required" db:"title"`
@@ -7,4 +9,17 @@ type Tasks struct {
 	Status      string `json:"status" db:"status"`
 	Created     string `json:"created_at" db:"created_at"`
 	Updated     string `json:"updated_at" db:"updated_at"`
+}
+
+type UpdateTaskInput struct {
+	Title       *string `json:"title" db:"title"`
+	Description *string `json:"description"  db:"description"`
+	Status      *string `json:"status" db:"status"`
+}
+
+func (u *UpdateTaskInput) Validate() error {
+	if u.Title == nil && u.Description == nil && u.Status == nil {
+		return errors.New("update structure has no values")
+	}
+	return nil
 }
