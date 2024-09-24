1bb72999e7cb78648e5121410a7ea513dc66086c
Jemal-bashimovna <rozynyazowajemal@gmail.com>
2024-09-24 15:56:57 +0500

    getTasks complited

--- pkg/repository/task_postgres.go
+++ pkg/repository/task_postgres.go
@@ -23,11 +23,24 @@ func (r *TaskPostgres) CreateTask(task taskmanagement.Tasks) (int, error) {
 	query := fmt.Sprintf("insert into %s (title, description) values ($1, $2) RETURNING id", tasksTable)
 
 	row := r.db.QueryRow(query, task.Title, task.Description)
-	fmt.Println(query)
+
 	if err := row.Scan(&id); err != nil {
 		log.Printf("failed to scan id : %v", err)
 		return 0, err
 	}
 
 	return id, nil
+}
+
+func (r *TaskPostgres) GetTasks() ([]taskmanagement.Tasks, error) {
+	var tasks []taskmanagement.Tasks
+
+	query := fmt.Sprintf("SELECT ts.id, ts.title, ts.description, ts.status, ts.created_at, ts.updated_at FROM %s ts", tasksTable)
+
+	err := r.db.Select(&tasks, query)
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
 }
